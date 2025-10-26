2204df51fc327e37aeebd438eaff31bdea34866c
filipsmolic <filip.smolic.zadar@gmail.com>
2025-10-26 14:01:38 +0100

    Fix logout redirect 2

--- loto_frontend/src/environments/environment.prod.ts
+++ loto_frontend/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
     domain: 'dev-xsbault4juhyeee0.eu.auth0.com',
     clientId: 'PI14wUOXsCJvcYe9289cxDJMfAGF9OI1',
     audience: 'https://my-loto-api/',
-    redirect_uri: 'https://loto-backend-zd7t.onrender.com',
+    redirect_uri: 'https://loto-frontend-site.onrender.com',
   },
   apiBaseUrl: 'https://loto-backend-zd7t.onrender.com',
 };
